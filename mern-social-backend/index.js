88cfd265114dd1dd49d69791d9838c1d1082840e
Muhammad Asif <asif88383@gmail.com>
2022-05-31 22:49:42 +0500

    first commit

--- mern-social-backend/index.js
+++ mern-social-backend/index.js
@@ -1,10 +1,19 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
-app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-mongoose.connect( process.env.MONGODB_LINK );
-
+mongoose
+  .connect(
+    process.env.MONGOGB_LiNK,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() =>
+    app.listen(5000, () => console.log("Server started on port 5000"))
+  );
