bfb5850ea4b1aa9d6db5f7570c4577286cb711cc
Muhammad Asif <asif88383@gmail.com>
2022-06-01 11:48:30 +0500

    first commit

--- mern-social-backend/Controllers/UserController.js
+++ mern-social-backend/Controllers/UserController.js
@@ -40,3 +40,23 @@ export const updateUser = async (req, res) => {
         res.status(401).json("Access Denied! You are not authorized to update this user");
     }
 }
+
+
+// delete a user
+export const deleteUser = async (req, res) => {
+    const id = req.params.id;
+
+    const {currentUserId, currentUserAdminStatus} = req.body;
+
+    if(currentUserId===id || currentUserAdminStatus){
+        try{
+            await UserModel.findByIdAndDelete(id);
+            res.status(200).json("User deleted successfully");
+        }catch(error){
+            res.status(500).json(error);
+        }
+    }else{
+        res.status(401).json("Access Denied! You are not authorized to delete this user");
+    }
+    
+}
