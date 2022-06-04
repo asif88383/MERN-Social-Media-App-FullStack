d9666c05af7959c31aa56641f1ed7f9e292d7757
Muhammad Asif <asif88383@gmail.com>
2022-06-04 19:03:15 +0500

    first commit

--- mern-social-backend/Controllers/PostController.js
+++ mern-social-backend/Controllers/PostController.js
@@ -62,3 +62,22 @@ export const deletePost = async(req, res) => {
     }
 }
 
+// like/ unlike a post
+export const likePost = async(req, res) => {
+    const id = req.params.id;
+    const {userId} = req.body;
+
+    try {
+        const post = await PostModel.findById(id);
+        if(!post.likes.includes(userId)){
+            await post.updateOne({$push: {likes: userId}});
+            res.status(200).json("Post liked successfully");
+        }else{
+            await post.updateOne({$pull: {likes: userId}});
+            res.status(200).json("Post unliked successfully");
+        }
+    }catch(err){
+        res.status(500).json(`Error liking post: ${err}`);
+    }
+}
+
