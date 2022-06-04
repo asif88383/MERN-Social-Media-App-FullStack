c2b71839d807a819d93fd4ee7e57f4f616f7baed
Muhammad Asif <asif88383@gmail.com>
2022-06-04 19:14:32 +0500

    first commit

--- mern-social-backend/Routes/AuthRoute.js
+++ mern-social-backend/Routes/AuthRoute.js
@@ -3,7 +3,6 @@ import { registerUser, loginUser } from "../Controllers/AuthController.js";
 
 const router = exporess.Router();
 
-router.get('/', async(req, res) => {res.send('Auth Route')});
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
