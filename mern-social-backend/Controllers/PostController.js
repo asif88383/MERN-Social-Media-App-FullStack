aa1d20c1f4676b5ea715268c25c0a20694f3154f
Muhammad Asif <asif88383@gmail.com>
2022-06-16 15:03:37 +0500

    first commit

--- mern-social-backend/Controllers/PostController.js
+++ mern-social-backend/Controllers/PostController.js
@@ -1,121 +1,123 @@
-import PostModel from "../Models/postModal";
+import PostModel from "../Models/postModel.js";
 import mongoose from "mongoose";
-import UserModal from "../Models/userModal";
+import UserModel from "../Models/userModel.js";
 
-// create new Post  
+// Creat new Post
 export const createPost = async (req, res) => {
-    const newPost = new PostModel(req.body);
+  const newPost = new PostModel(req.body);
 
-    try {
-        await newPost.save();;
-        res.status(200).json("Post created successfully");
-    }catch(err){
-        res.status(500).json(`Error creating post: ${err}`);
-    }
-}
+  try {
+    await newPost.save();
+    res.status(200).json("Post created!");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-// Get a post 
+// Get a post
+
 export const getPost = async (req, res) => {
-    const postId = req.params.id;
+  const id = req.params.id;
 
-    try {
-        const post = await PostModel.findById(postId);
-        res.status(200).json(post);
-    }catch(err){
-        res.status(500).json(`Error getting post: ${err}`);
+  try {
+    const post = await PostModel.findById(id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// Update a post
+export const updatePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (post.userId === userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Post Updated");
+    } else {
+      res.status(403).json("Action forbidden");
     }
-}
-
-// Update a post 
-export const updatePost = async(req, res) => {
-    const postId = req.params.id;
-    const {userId} = req.body;
-
-    try {
-        const post = await PostModel.findById(postId);
-        if(post.userId === userId){
-            await post.updateOne({$set: req.body});
-            res.status(200).json("Post updated successfully");
-        }else{
-            res.status(401).json("Unauthorized");
-        }
-    }catch(err){
-        res.status(500).json(`Error updating post: ${err}`);
-    }
-}
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 // Delete a post
-export const deletePost = async(req, res) => {
-    const id = req.params.id;
-    const {userId} = req.body;
+export const deletePost = async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.body;
 
-    try {
-        const post = await PostModel.findById(id);
-        if(post.userId === userId) {
-            await post.deleteOne();
-            res.status(200).json("Post deleted successfully");
-        } else {
-            res.status(401).json("Unauthorized");
-        }
-    }catch(err){
-        res.status(500).json(`Error deleting post: ${err}`);
+  try {
+    const post = await PostModel.findById(id);
+    if (post.userId === userId) {
+      await post.deleteOne();
+      res.status(200).json("POst deleted successfully");
+    } else {
+      res.status(403).json("Action forbidden");
     }
-}
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-// like/ unlike a post
-export const likePost = async(req, res) => {
-    const id = req.params.id;
-    const {userId} = req.body;
+// like/dislike a post
+export const likePost = async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.body;
 
-    try {
-        const post = await PostModel.findById(id);
-        if(!post.likes.includes(userId)){
-            await post.updateOne({$push: {likes: userId}});
-            res.status(200).json("Post liked successfully");
-        }else{
-            await post.updateOne({$pull: {likes: userId}});
-            res.status(200).json("Post unliked successfully");
-        }
-    }catch(err){
-        res.status(500).json(`Error liking post: ${err}`);
+  try {
+    const post = await PostModel.findById(id);
+    if (!post.likes.includes(userId)) {
+      await post.updateOne({ $push: { likes: userId } });
+      res.status(200).json("Post liked");
+    } else {
+      await post.updateOne({ $pull: { likes: userId } });
+      res.status(200).json("Post Unliked");
     }
-}
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-// Get Timeline posts 
-export const getTimelinePosts = async(req, res) => {
-    const userId = req.params.id;
+// Get Timeline POsts
+export const getTimelinePosts = async (req, res) => {
+  const userId = req.params.id;
 
-    try {
-        const currentUserPosts = await PostModel.find({userId: userId});
-        const followingPosts = await PostModel.aggregate([
-            {
-                $match:{
-                    _id: new mongoose.Types.ObjectId(userId),
-                },
-            },
-            {
-                $lookup:{
-                    from: "posts",
-                    localField: "following",
-                    foreignField: "userId",
-                    as: "followingPosts",
-                },
-            },
-            {
-                $project: {
-                    followingPosts: 1,
-                    _id: 0,
-                },
-            },
-        ]);
+  try {
+    const currentUserPosts = await PostModel.find({ userId: userId });
+    const followingPosts = await UserModel.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: "posts",
+          localField: "following",
+          foreignField: "userId",
+          as: "followingPosts",
+        },
+      },
+      {
+        $project: {
+          followingPosts: 1,
+          _id: 0,
+        },
+      },
+    ]);
 
-        res.status(200).json(
-          currentUserPosts
-            .concat(...followingPosts[0].followingPosts)
-            .sort((a, b) => b.createdAt - a.createdAt)
-        );
-    }catch(err){
-        res.status(500).json(`Error getting timeline posts: ${err}`);
-    }
-}
-
+    res.status(200).json(
+      currentUserPosts
+        .concat(...followingPosts[0].followingPosts)
+        .sort((a, b) => {
+          return b.createdAt - a.createdAt;
+        })
+    );
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
