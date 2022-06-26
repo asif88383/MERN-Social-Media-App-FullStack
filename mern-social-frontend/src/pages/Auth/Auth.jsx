08bb2e8a46f6630f999a45fead5fe21779fda930
Muhammad Asif <asif88383@gmail.com>
2022-06-27 01:17:02 +0500

    redux configured

--- mern-social-frontend/src/pages/Auth/Auth.jsx
+++ mern-social-frontend/src/pages/Auth/Auth.jsx
@@ -4,6 +4,39 @@ import Logo from "../../img/logo.png";
 
 const Auth = () => {
   const [isSignUp, SetIsSignUp] = useState(false);
+  const [data, setData] = useState({
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
+    confirmpassword: "",
+  });
+  const [confirmPassword, setConfirmPass] = useState(false);
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isSignUp) {
+      if (data.password !== data.confirmpassword) {
+        setConfirmPass(false);
+      }
+    }
+  };
+
+  const resetForm = () => {
+    setConfirmPass(true);
+    setData({
+      firstname: "",
+      lastname: "",
+      username: "",
+      password: "",
+      confirmpassword: "",
+    });
+  };
 
   return (
     <div className="Auth">
@@ -18,7 +51,7 @@ const Auth = () => {
 
       {/* right side */}
       <div className="a-right">
-        <form action="" className="infoForm authForm">
+        <form action="" className="infoForm authForm" onSubmit={handleSubmit}>
           <h3>{isSignUp ? "Sign up" : "Log in"}</h3>
 
           {isSignUp && (
@@ -28,12 +61,16 @@ const Auth = () => {
                 className="infoInput"
                 placeholder="First Name"
                 name="firstname"
+                onChange={handleChange}
+                value={data.firstname}
               />
               <input
                 type="text"
                 className="infoInput"
                 placeholder="Last Name"
                 name="lastname"
+                onChange={handleChange}
+                value={data.lastname}
               />
             </div>
           )}
@@ -44,6 +81,8 @@ const Auth = () => {
               className="infoInput"
               placeholder="Username"
               name="username"
+              onChange={handleChange}
+              value={data.username}
             />
           </div>
 
@@ -53,6 +92,8 @@ const Auth = () => {
               className="infoInput"
               placeholder="Password"
               name="password"
+              onChange={handleChange}
+              value={data.password}
             />
             {isSignUp && (
               <input
@@ -60,17 +101,38 @@ const Auth = () => {
                 className="infoInput"
                 placeholder="Confirm Password"
                 name="confirmpassword"
+                onChange={handleChange}
+                value={data.confirmpassword}
               />
             )}
           </div>
+          <span
+            style={{
+              display: confirmPassword ? "none" : "block",
+              color: "red",
+              fontSize: "12px",
+              alignSelf: "flex-end",
+              marginRight: "5px",
+            }}
+          >
+            *Confirm Password is not same!
+          </span>
 
           <div>
-            <span style={{ fontSize: "12px" }}>
-              Already have an Account? <a href="/signin">Log In</a>
+            <span
+              style={{ fontSize: "12px", cursor: "pointer" }}
+              onClick={() => {
+                SetIsSignUp((prev) => !prev);
+                resetForm();
+              }}
+            >
+              {isSignUp
+                ? "Already have an Account? Log In"
+                : "Don't have an account? Sign Up"}
             </span>
           </div>
           <button type="submit" className="infoButton button">
-            {isSignUp? "Sign Up" : "Log In"}
+            {isSignUp ? "Sign Up" : "Log In"}
           </button>
         </form>
       </div>
